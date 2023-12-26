cd8706f2b749d8c0187461211e2442959fc79e9a
dumsterB <dumsterfordevelop@gmail.com>
2023-12-26 18:39:43 +0500

    fix: add hashservice module to company

--- src/company/company.module.ts
+++ src/company/company.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { CompanyController } from './company.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CompanyService } from './company.service';
+import { HashService } from './hash.service';
 
 @Module({
     imports: [PrismaModule],
-    providers: [CompanyService],
-    exports: [CompanyService],
+    providers: [CompanyService,HashService],
+    exports: [CompanyService,HashService],
     controllers: [CompanyController],
 })
 export class CompanyModule {}
